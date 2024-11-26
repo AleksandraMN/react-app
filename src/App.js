5ace6e6d33332f366323f1231cb1e8d6303369ee
AleksandraMN <al_mn@mail.ru>
2024-11-26 20:49:04 +1000

    Not JSX

--- src/App.js
+++ src/App.js
@@ -1,5 +1,64 @@
 import logo from './logo.svg';  /*  Императивный стиль */
 import './App.css';
+import React from 'react';
+
+
+export const App = () => {
+  return React.createElement(
+    "div",
+    {
+      className: "App"
+    },
+    React.createElement(
+      "header",
+      {
+        className: "App-header"
+      },
+      React.createElement("img", {
+        src: logo,
+        className: "App-logo",
+        alt: "logo"
+      }),
+      React.createElement(
+        "p",
+        null,
+        "Edit ",
+        React.createElement("code", null, "src/App.js"),
+        " and save to reload."
+      ),
+      React.createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer"
+        },
+        "Learn React"
+      )
+    ),
+    React.createElement(
+      "footer",
+      null,
+      React.createElement(
+        "time",
+        {
+          className: "today_year"
+        },
+        new Date().getFullYear(),
+        "  "
+      )
+    )
+  );
+};
+
+
+
+
+
+
+
+/*
 
 export const App = () => {
 
@@ -21,7 +80,7 @@ export const App = () => {
 			</header>
 			<footer>
 				<time className="today_year">
-				  {new Date().getFullYear()}  {/*  Декларативный  стиль */}
+				  {new Date().getFullYear()}    Декларативный  стиль
 				</time>
 			</footer>
 		</div>
@@ -29,3 +88,4 @@ export const App = () => {
 };
 
 
+ */
