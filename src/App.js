5c3728723070c4cebc55b8c4a63c210ab319f7f8
AleksandraMN <al_mn@mail.ru>
2024-11-26 20:51:20 +1000

    With JSX

--- src/App.js
+++ src/App.js
@@ -1,67 +1,7 @@
 import logo from './logo.svg';  /*  Императивный стиль */
 import './App.css';
-import React from 'react';
-
 
 export const App = () => {
-  return React.createElement(
-    "div",
-    {
-      className: "App"
-    },
-    React.createElement(
-      "header",
-      {
-        className: "App-header"
-      },
-      React.createElement("img", {
-        src: logo,
-        className: "App-logo",
-        alt: "logo"
-      }),
-      React.createElement(
-        "p",
-        null,
-        "Edit ",
-        React.createElement("code", null, "src/App.js"),
-        " and save to reload."
-      ),
-      React.createElement(
-        "a",
-        {
-          className: "App-link",
-          href: "https://reactjs.org",
-          target: "_blank",
-          rel: "noopener noreferrer"
-        },
-        "Learn React"
-      )
-    ),
-    React.createElement(
-      "footer",
-      null,
-      React.createElement(
-        "time",
-        {
-          className: "today_year"
-        },
-        new Date().getFullYear(),
-        "  "
-      )
-    )
-  );
-};
-
-
-
-
-
-
-
-/*
-
-export const App = () => {
-
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -80,7 +20,7 @@ export const App = () => {
 			</header>
 			<footer>
 				<time className="today_year">
-				  {new Date().getFullYear()}    Декларативный  стиль
+				  {new Date().getFullYear()}    {/* Декларативный  стиль */}
 				</time>
 			</footer>
 		</div>
@@ -88,4 +28,3 @@ export const App = () => {
 };
 
 
- */
